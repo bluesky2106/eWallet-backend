b8bfe5d2458380017ad7c9cbe11799803f8dd8a4
bluesky2106 <hoaiphuong.nguyen.vn.2106@gmail.com>
2020-03-23 17:27:01 +0700

    [models] update user

--- models/user.go
+++ models/user.go
@@ -6,12 +6,13 @@ import (
 
 // User : struct
 type User struct {
-	gorm.Model
+	ID                 uint
 	FullName           string
 	UserName           string
 	Email              string `gorm:"unique_index"`
 	Password           string
 	Keystore           string
+	CryptoPassphase    string
 	EnableNotification bool `gorm:"default:true"`
 	// UserWallets        []Wallet `gorm:"foreignkey:UserID;auto_preload:true"`
 }
@@ -25,6 +26,20 @@ type UserPassword struct {
 	Code    string
 	Expired uint64
 	Retry   uint `gorm:"default:0"`
+}
+
+// UserRegisterReq : user register request
+type UserRegisterReq struct {
+	FullName        string `json:"FullName"`
+	Email           string `json:"Email"`
+	Password        string `json:"Password"`
+	ConfirmPassword string `json:"ConfirmPassword"`
+}
+
+// UserLoginReq : struct
+type UserLoginReq struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 // UserLoginResp : struct
