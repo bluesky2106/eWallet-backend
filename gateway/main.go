ff49656b76e999b3f9ab95689c388a3a09843772
bluesky2106 <hoaiphuong.nguyen.vn.2106@gmail.com>
2020-03-23 17:27:58 +0700

    add main.go

---
+++ gateway/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	commonConfig "github.com/bluesky2106/eWallet-backend/config"
+	"github.com/bluesky2106/eWallet-backend/gateway/api"
+	"github.com/bluesky2106/eWallet-backend/gateway/config"
+	"github.com/bluesky2106/eWallet-backend/gateway/services"
+	"github.com/bluesky2106/eWallet-backend/log"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+var logger *zap.Logger
+
+func main() {
+	conf := commonConfig.ParseConfig("config.json", "../config")
+	conf.Print()
+	logger = log.InitLogger(conf.Env)
+
+	gwConf := &config.Config{
+		Env:                string(conf.Env),
+		Host:               conf.APIGateway.Host,
+		Port:               conf.APIGateway.Port,
+		EntryCacheEndpoint: fmt.Sprintf("%s:%s", conf.EntryCache.Host, conf.EntryCache.Port),
+		TokenSecretKey:     conf.TokenSecretKey,
+		CryptoPassphase:    conf.CryptoPassphase,
+	}
+	productSrv := services.NewProductService(gwConf)
+	userSrv := services.NewUserService(gwConf)
+
+	router := gin.New()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://*", "https://*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc:  func(origin string) bool { return true },
+		AllowMethods:     []string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		MaxAge:           12 * time.Hour,
+	}))
+
+	svr := api.NewServer(gwConf, router, productSrv, userSrv)
+	authMw := api.AuthMiddleware(string(conf.TokenSecretKey), svr.Authenticate)
+	svr.Routes(authMw)
+	if err := router.Run(fmt.Sprintf("%s:%s", gwConf.Host, gwConf.Port)); err != nil {
+		logger.Error("router.Run", zap.Error(err))
+	}
+}
